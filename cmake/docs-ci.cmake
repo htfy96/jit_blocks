b84d8bd55638af79f4ba38c8371592abb3e8bcfe
Vic Luo <vicluo96@gmail.com>
2024-10-20 23:53:21 -0400

    fix docs-ci

--- cmake/docs-ci.cmake
+++ cmake/docs-ci.cmake
@@ -94,6 +94,8 @@ set(out "${DOXYGEN_OUTPUT_DIRECTORY}")
 
 set(SOURCE_DOC_CONFIG_DIR "${PROJECT_SOURCE_DIR}/docs")
 
+set(AWESOME_CSS_DIR ${doxygen_awesome_SOURCE_DIR})
+
 foreach(file IN ITEMS Doxyfile conf.py)
   configure_file("${src}/docs/${file}.in" "${bin}/docs/${file}" @ONLY)
 endforeach()
